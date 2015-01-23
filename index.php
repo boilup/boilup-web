68661de2119e2e6cf4809e8deb0616e2ca6f517c
root <root@tooth.whiteco.int>
2015-01-23 18:06:22 -0500

    adding base image for the boilup website. please be gentle

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE htm
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>boilup.ca</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="starter-template.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">boilup.ca</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#monitoring">Monitoring</a></li>
+            <li><a href="#vintage">Vintage bikes</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="mileage.php">Gas mileage</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+ 
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Hey, how about a cup of tea and a cut of pie?</h1>
+        <img  src="http://photos-d.ak.instagram.com/hphotos-ak-xpf1/10507795_606187919479659_1684097452_n.jpg" class="img-responsive center-block" src="//instagram.com/p/p2Rc_XyQ2o/embed/" alt="Responsive image">
+
+        <!-- <iframe src="//instagram.com/p/p2Rc_XyQ2o/embed/" frameborder="1" scrolling="yes" allowtransparency="true"></iframe>  ->
+        <!-- <iframe src="//instagram.com/p/p2Rc_XyQ2o/embed/" width="612" height="710" frameborder="0" scrolling="no" allowtransparency="true"></iframe>  -->
+        <p class="lead">Tastes great, less filling... Welcome to boilup.ca, where lack of punctuation makes a statement.<br></p>
+        <p >This is just a page about things I'm into, an hopefully not a starting place for my identitfy theft. Working in IT but dabbling in lots of things, including the family, bikes, guitars and yep tech. Have a look around, as this might not be here forever OOOOooooo....<br></p>
+        <a name='monitoring'>
+        <h2>Monitoring section</h2></a>
+	This will be where i'm gong to talk about my monitoring tools i've used over the years, not unlike my resume (see contacts). I've had my hands on a broad variety of monitoring tools, from home-brew setups to full blown Enterprise sized solutions.  Cobbling a list together takes days, that's why it's not here yet, but will be very soon!
+        <a name='vintage'>
+        <h2>Vintage Motorcycle</h2></a>
+	<p>Vintage motorcycles, more specifically Yamaha are a passion of mine. Ever since i was a teenager in high school I've watched and drooled as the older kids with their license would zoom around. Something about the speed, sound, smell, fun and to a lesser extent, risk always drew me to bikes and recreational equipment in general.I've been dabbling with My 1981 Seca 750 for about 3 years and it's been a labour love, sorta. Any motorcycle (vehicle) over 10 years of age is definitley needing a going over, nevermind all the basics.  I'm no mechanic but tearing into a set of carburetors no longer scares me! Here's the photo album of my progress and end result:</p>
+
+<div class="panel panel-primary">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Motorcycle Albums</div>
+
+  <!-- Table -->
+<table class="table" style="width:194px;" align="center" >
+<tr>
+<td align="center" style="height:194px;background:url(https://www.gstatic.com/pwa/s/v/lighthousefe_20140824.00_p2/transparent_album_background.gif) no-repeat left"><a href="https://picasaweb.google.com/110534603428796472381/1981750Seca?authuser=0&feat=embedwebsite"><img src="https://lh4.googleusercontent.com/-D5rtAm2Z7PY/UFX6mklBoeE/AAAAAAAAB8k/cPVDq3HRJ4k/s160-c/1981750Seca.jpg" width="160" height="160" style="margin:1px 0 0 4px;"></a>
+</td>
+</tr>
+<tr>
+<td style="text-align:center;font-family:arial,sans-serif;font-size:11px"><a href="https://picasaweb.google.com/110534603428796472381/1981750Seca?authuser=0&feat=embedwebsite" style="color:#4D4D4D;font-weight:bold;text-decoration:none;">1981 750 Seca</a>
+</td>
+</tr>
+</table>
+</div>
+
+
+
+
+        <a name='about'>
+        <h2>About</h2></a>
+	So far it's been a complete loop from newfoundland to ontario and back again (sorta). Originally from Kippens, Newfoundland and Labrador and curently living in St John's. Great city, I hear the lowest rates of skin cancer here on account of all the fog ;). 
+        <a name='contact'>
+        <h2>Contact Info</h2></a>
+        <p>Please feel free to contact me via linkedin or instagram if you can figure that out (I'm not entirely sure how to do it =D ). </p> 
+	<a href="http://ca.linkedin.com/pub/marcel-white/3/a72/0"><img src="https://dlc1-s.licdn.com/sites/default/files/Logo-21px-R.png" alt="linkedin"></a>
+        <a href="http://instagram.com/marcelwhite74?ref=badge" class="ig-b- ig-b-v-24"><img src="http://badges.instagram.com/static/images/ig-badge-view-24.png" alt="Instagram"></a>
+      </div>
+
+    </div>
+    
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
