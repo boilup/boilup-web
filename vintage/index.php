165126ab8ce5068e6254f202f62ac788b8b08b1e
unknown <whiteco@severe.whiteco.int>
2015-01-28 23:08:47 -0330

    updating the bike table for a laugh

--- vintage/index.php
+++ vintage/index.php
@@ -1,57 +1,67 @@
 <!DOCTYPE htm
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>boilup.ca</title>
-    <?php include '../bstop.inc';?>
-  </head>
-  <body>
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">boilup.ca</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <?php include('../menu.inc');?>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
- 
+  <html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>boilup.ca</title>
+  <?php include '../bstop.inc';?>
+</head>
+<body>
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
-
-      <div class="starter-template">
-        <a name='vintage'>
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">boilup.ca</a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <?php include('../menu.inc');?>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
+  <div class="container">
+    <div class="starter-template">
+      <a name='vintage'>
         <h2>Vintage Motorcycle</h2></a>
-	<p>Vintage motorcycles, more specifically Yamaha are a passion of mine. Ever since i was a teenager in high school I've watched and drooled as the older kids with their license would zoom around. Something about the speed, sound, smell, fun and to a lesser extent, risk always drew me to bikes and recreational equipment in general.I've been dabbling with My 1981 Seca 750 for about 3 years and it's been a labour love, sorta. Any motorcycle (vehicle) over 10 years of age is definitley needing a going over, nevermind all the basics.  I'm no mechanic but tearing into a set of carburetors no longer scares me! Here's the photo album of my progress and end result:</p>
-<hr>
-<h3>1981 Yamaha XJ750RL Seca</h3>
-<p>Here's my current ride. Interesting story on this one, as it was the product of the insurance claim payout and some sleuthing on Kijiji in Southwestern Ontario. It's had a bit of a checkered past, originally a New Hampshire Bike, the previous owner had originally intended to create a rat bike out of it, but in reality it just sat in his barn for a few years once he got a cruiser.  The main problem was a bum idle circuit on the 1st cylinder (only idled on 3 cylinders). </p>
-<iframe class="imgur-album" width="100%" height="550" frameborder="0" src="//imgur.com/a/5MXbJ/embed"></iframe>
+        <p>Vintage motorcycles, more specifically Yamaha are a passion of mine. Ever since i was a teenager in high school I've watched and drooled as the older kids with their license would zoom around. Something about the speed, sound, smell, fun and to a lesser extent, risk always drew me to bikes and recreational equipment in general.I've been dabbling with My 1981 Seca 750 for about 3 years and it's been a labour love, sorta. Any motorcycle (vehicle) over 10 years of age is definitley needing a going over, nevermind all the basics.  I'm no mechanic but tearing into a set of carburetors no longer scares me! Here's the photo album of my progress and end result:</p>
+        <hr>
+        <h3>1981 Yamaha XJ750RL Seca</h3>
+        <p>Here's my current ride. Interesting story on this one, as it was the product of the insurance claim payout and some sleuthing on Kijiji in Southwestern Ontario. It's had a bit of a checkered past, originally a New Hampshire Bike, the previous owner had originally intended to create a rat bike out of it, but in reality it just sat in his barn for a few years once he got a cruiser.  The main problem was a bum idle circuit on the 1st cylinder (only idled on 3 cylinders).</p>
+        <iframe class="imgur-album" width="100%" height="550" frameborder="0" src="//imgur.com/a/5MXbJ/embed"></iframe>
+        <p>Here's a list of *accomplishments* so far to it
+          <div class="panel panel-default">
+            <!-- Default panel contents -->
+            <div class="panel-heading">Updates</div>
+            <div class="panel-body">
+              <p>...</p>
+            </div>
 
-<hr>
-<h3>1983 Yamaha XJ650K Maxim</h3>
-<p>Here's my previous (and subsequent donor) bike. She was a good steed and my first forray in to motorcycles, period. Never knew what I was getting myself into, but i had way too much fun as it were. The funny thing is that my son and I were out to pickup two angel food cake mixes when the accident happened, that's what saved us no doubt!</p>
-<iframe class="imgur-album" width="100%" height="550" frameborder="0" src="//imgur.com/a/0zjID/embed"></iframe>
+            <!-- Table -->
+            <table class="table">
+              <tr>
+                <td>testing 1 2 3 table data</td>  
+              </tr>
+            </table>
+          </div>
+        </p>
 
-
+        <hr>
+        <h3>1983 Yamaha XJ650K Maxim</h3>
+        <p>Here's my previous (and subsequent donor) bike. She was a good steed and my first forray in to motorcycles, period. Never knew what I was getting myself into, but i had way too much fun as it were. The funny thing is that my son and I were out to pickup two angel food cake mixes when the accident happened, that's what saved us no doubt!</p>
+        <iframe class="imgur-album" width="100%" height="550" frameborder="0" src="//imgur.com/a/0zjID/embed"></iframe>
       </div>
     </div>
     
-
-
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="../dist/js/bootstrap.min.js"></script>
   </body>
-</html>
+  </html>
