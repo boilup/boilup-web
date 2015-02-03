e8c86533e55d663b81aeb2d133f36ce8cbae4ca9
root <root@tooth.whiteco.int>
2015-02-02 19:18:54 -0500

    changed theme and updated the default navbar css reference

---
+++ test/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE htm>
+<html>
+  <head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>boilup.ca</title><?php include('../bstop.inc');?>
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+        </button> 
+        <a class="navbar-brand" href="/">boilup.ca</a></div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <?php include('../menu.inc');?>
+          </ul>
+        </div>
+        <!--/.nav-collapse -->
+      </div>
+    </div>
+    <div class="container">
+    <div class="starter-template">
+    <a name='monitoring'>
+      <h2>Monitoring</h2>
+    </a>
+    <p>Here goes. For the better part of my technology career I&#39;ve been working with monitoring tools in some way, shape or
+    form. A few different quotes come to mind:</p>
+    <div class="panel panel-default">
+      <div class="panel-body">&quot;Knowing is always better than not knowing.&quot;,
+      <br />&quot;It&#39;s always better for you to realize something of yours is down before being told as much.&quot;
+      <br />and to a lesser extent &quot;If you wait until the last minute it only takes a minute to do.&quot;
+      <br /></div>
+    </div>Now while I&#39;d rather this not be my resume per say, here are some of the tools I&#39;ve had the pleasure of using
+    over the years. These have been used in small -&gt; enterprise class environments in a global setting. Lot&#39;s of fun!
+    <div class="panel panel-default">
+      <!-- Default panel contents -->
+      <div class="panel-heading">Monitoring</div>
+      <div class="panel-body">
+        <p>...</p>
+      </div>
+      <ul class="list-group">
+        <li class="list-group-item">http://www.nagios.org</li>
+        <li class="list-group-item">http://www.ca.com/ca/en/opscenter/ca-ehealth.aspx</li>
+        <li class="list-group-item">http://www.netitive.com/</li>
+        <li class="list-group-item">http://canada.emc.com/it-management/smarts/index.htm</li>
+        <li class="list-group-item">CA Sysedge (snmpd client)</li>
+      </ul>
+    </div>
+    <div class="panel panel-default">
+    <!-- Default panel contents -->
+    <div class="panel-heading">Panel heading</div>
+    <!-- Table -->
+    ...
+    <table class="table"></table></div></div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+     
+    <script src="../dist/js/bootstrap.min.js"></script></div>
+  </body>
+</html>
+
